ef6756bdfcad05b1ae3b3cd92a0670ebe8f4765e
Larissa-Galdino <larigaldino.96@gmail.com>
2025-12-23 20:51:29 -0300

    Update EmpresaDTO.cs

--- DTOs/EmpresaDTO.cs
+++ DTOs/EmpresaDTO.cs
@@ -2,10 +2,10 @@ namespace TrabalhoApi.DTOs
 {
     public class EmpresaDTO
     {
-        public string Nome { get; set; }
-        public string Cnpj { get; set; }
-        public string Setor { get; set; }
-        public string Endereco { get; set; }
+        public string? Nome { get; set; }
+        public string? Cnpj { get; set; }
+        public string? Setor { get; set; }
+        public string? Endereco { get; set; }
         public DateTime DataFundacao { get; set; }
     }
 }
